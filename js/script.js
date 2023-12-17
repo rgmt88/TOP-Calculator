d04a1de2242c5abf06c51860f7ef0bc46a4d103f
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-16 23:52:52 -0500

    Completed numbers to be displayed in calculator

--- js/script.js
+++ js/script.js
@@ -36,3 +36,34 @@ function operate(operator, num1, num2) {
     }
 }
 
+function setupCalculator() {
+    let currentValue = ""; // Variable to hold the current value
+
+    // Function to handle number button press
+    function numberPressed(number) {
+        currentValue += number; // Append the pressed number to the current value
+        document.getElementById('result-display').innerText = currentValue; // Display the current value
+    }
+
+    // Setup event listeners for number buttons
+    const numberButtons = document.querySelectorAll('.numbers');
+    numberButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            numberPressed(this.value);
+        });
+    });
+
+    //Clear button
+    const clearButton = document.getElementById('clear-button');
+    clearButton.addEventListener('click', function() {
+        document.getElementById('result-display').innerText = 0;
+        currentValue = "";
+    });
+
+
+}
+
+// Call setupCalculator when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', setupCalculator);
+
+
