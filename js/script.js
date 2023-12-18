471bc920c329fcddf075e00133c98afa4f799336
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-18 01:02:33 -0500

    Added function setupCalculator, manages all calc

--- js/script.js
+++ js/script.js
@@ -12,14 +12,11 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        throw new Error("Division by zero is not allowed");
+        alert("Division by zero is not allowed")
+        return 0;
     }
     return num1 / num2;
 };
-
-let num1 = 0;
-let num2 = 0;
-let operator = '';
 
 function operate(operator, num1, num2) {
     switch (operator) {
@@ -37,33 +34,79 @@ function operate(operator, num1, num2) {
 }
 
 function setupCalculator() {
-    let currentValue = ""; // Variable to hold the current value
+    let currentValue = ""; 
+    let num1 = null;
+    let currentOperator = '';
+    let calculationHistory = ""; 
 
-    // Function to handle number button press
-    function numberPressed(number) {
-        currentValue += number; // Append the pressed number to the current value
-        document.getElementById('result-display').innerText = currentValue; // Display the current value
-    }
+    function performCalculation() {
+        if (num1 !== null && currentValue !== "" && currentOperator) {
+            let num2 = parseFloat(currentValue);
+            let result = operate(currentOperator, num1, num2);
 
-    // Setup event listeners for number buttons
+            let formatter = new Intl.NumberFormat('en-US', {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            });
+
+            document.getElementById('result-display').innerText = formatter.format(result);
+            calculationHistory += ` ${currentValue} = ${result}`;
+            console.log(calculationHistory); 
+            num1 = result; 
+            currentValue = ""; 
+        } 
+    };
+
     const numberButtons = document.querySelectorAll('.numbers');
     numberButtons.forEach(button => {
         button.addEventListener('click', function() {
-            numberPressed(this.value);
+            currentValue += this.value;
+            document.getElementById('result-display').innerText = currentValue;
         });
     });
 
-    //Clear button
     const clearButton = document.getElementById('clear-button');
     clearButton.addEventListener('click', function() {
-        document.getElementById('result-display').innerText = 0;
         currentValue = "";
+        num1 = null;
+        currentOperator = '';
+        calculationHistory = "";
+        document.getElementById('result-display').innerText = "0.00";
+        console.log("Calculator cleared"); 
     });
 
+    const equalButton = document.querySelector('.equal');
+    equalButton.addEventListener('click', function() {
+        performCalculation();
+        currentOperator = '';
+        calculationHistory = `${num1}`; // Start a new history with the last result
+    });
 
+    const operatorsButtons = document.querySelectorAll('.operators');
+    operatorsButtons.forEach(operator => {
+        operator.addEventListener('click', function() {
+            if (currentValue !== "") {
+                if (num1 === null) {
+                    num1 = parseFloat(currentValue);
+                    calculationHistory = `${num1}`;
+                } else if (currentOperator) {
+                    performCalculation();
+                }
+                currentOperator = operator.value;
+                calculationHistory += ` ${currentOperator}`;
+                console.log(calculationHistory);
+                currentValue = "";
+            } else if (num1 !== null && !currentValue) {
+                currentOperator = operator.value;
+                calculationHistory = `${num1} ${currentOperator}`;
+                console.log(calculationHistory); 
+            }
+        });
+    });
 }
 
-// Call setupCalculator when the DOM is fully loaded
 document.addEventListener('DOMContentLoaded', setupCalculator);
+
+
 
 
