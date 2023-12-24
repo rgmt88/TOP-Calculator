79428db5aed31ffea864dd7c841dcea6efcc5f44
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-23 20:23:58 -0500

    Final version

--- js/script.js
+++ js/script.js
@@ -43,28 +43,41 @@ function setupCalculator() {
     let num1 = null;
     let currentOperator = '';
     let calculationHistory = ""; 
+    let lastOperationCompleted = false;
 
     function performCalculation() {
         if (num1 !== null && currentValue !== "" && currentOperator) {
             let num2 = parseFloat(currentValue);
             let result = operate(currentOperator, num1, num2);
 
-            num1 = result;
-            
             document.getElementById('result-display').innerText = formatter.format(result);
-            calculationHistory += ` ${formatter.format(currentValue)} = ${formatter.format(result)}`;
+            calculationHistory = `${formatter.format(num1)} ${currentOperator} ${formatter.format(num2)} = `;
             document.getElementById('calc-history').innerText = calculationHistory;
             console.log(calculationHistory);  
+            
+            num1 = result;  // Store the raw result for further calculations
             currentValue = ""; 
-            currentOperator = '';
+            lastOperationCompleted = true;
         } 
-    };
+    }
 
     const numberButtons = document.querySelectorAll('.numbers');
     numberButtons.forEach(button => {
         button.addEventListener('click', function() {
+            if (lastOperationCompleted) {
+                currentValue = "";
+                num1 = null;
+                lastOperationCompleted = false;
+                calculationHistory = "";
+            }
+            
+            // Check if the button pressed is the decimal button
+            if (this.value === "." && currentValue.includes(".")) {
+                return;
+            };
+
             currentValue += this.value;
-            document.getElementById('result-display').innerText = formatter.format(currentValue);
+            document.getElementById('result-display').innerText = currentValue;
         });
     });
 
@@ -81,66 +94,72 @@ function setupCalculator() {
 
     const equalButton = document.querySelector('.equal');
     equalButton.addEventListener('click', function() {
-        performCalculation();
-        currentOperator = '';
-        calculationHistory = `${formatter.format(num1)}`; // Start a new history with the last result
+        if (currentOperator && currentValue !== "" && num1 !== null) {
+            performCalculation();
+            currentOperator = '';
+            lastOperationCompleted = true;
+            calculationHistory = ""; // Reset history after the calculation is complete
+        }
     });
 
     const operatorsButtons = document.querySelectorAll('.operators');
     operatorsButtons.forEach(operator => {
         operator.addEventListener('click', function() {
-            if (currentValue !== "") {
+            if (!lastOperationCompleted && currentValue !== "") {
                 if (num1 === null) {
                     num1 = parseFloat(currentValue);
-                    calculationHistory = `${formatter.format(num1)}`;
                 } else if (currentOperator) {
                     performCalculation();
                 }
-                currentOperator = operator.value;
-                calculationHistory += ` ${currentOperator}`;
-                document.getElementById('calc-history').innerText = calculationHistory;
-                console.log(calculationHistory);
-                currentValue = "";
-            } else if (num1 !== null && !currentValue) {
-                currentOperator = operator.value;
-                calculationHistory = `${formatter.format(num1)} ${currentOperator}`;
-                document.getElementById('calc-history').innerText = calculationHistory;
-                console.log(calculationHistory); 
+                calculationHistory = "";
             }
+            currentOperator = operator.value;
+            lastOperationCompleted = false;
+            calculationHistory += currentValue !== "" ? ` ${currentValue} ${currentOperator}` : ` ${currentOperator}`;
+            document.getElementById('calc-history').innerText = calculationHistory;
+            currentValue = "";
         });
     });
 
     const plusMinusButton = document.getElementById('pos-neg-button');
     plusMinusButton.addEventListener('click', function() {
-        if (num1 !== null && !currentValue) {
-            let valueChange = formatter.format(num1 * -1);
-            document.getElementById('result-display').innerText = valueChange;
-            num1 = parseFloat(valueChange);
-            calculationHistory = `${num1}`;    
-        } else {
-            currentValue = formatter.format(currentValue * -1);
-            document.getElementById('result-display').innerText = currentValue;
-            num1 = parseFloat(currentValue);
-            calculationHistory = `${num1}`;
-        };
+        if (currentValue) {
+            // If there is a current value entered, change its sign
+            currentValue = (parseFloat(currentValue) * -1).toString();
+            document.getElementById('result-display').innerText = formatter.format(parseFloat(currentValue));
+        } else if (num1 !== null) {
+            // If no current value but there is a calculated result, change the sign of the result
+            num1 = num1 * -1;
+            document.getElementById('result-display').innerText = formatter.format(num1);
+        }
+        // Update the calculation history only if needed
     });
 
     const percentageButton = document.getElementById('percentage-button');
     percentageButton.addEventListener('click', function() {
-        if (num1 !== null && !currentValue) {
-            let valueChange = formatter.format(num1 / 100);
-            document.getElementById('result-display').innerText = valueChange;
-
-            num1 = parseFloat(valueChange);
-            calculationHistory = `${num1}`;
-        } else {
-            currentValue = formatter.format(currentValue / 100);
-            document.getElementById('result-display').innerText = currentValue;
-            num1 = parseFloat(currentValue);
-            calculationHistory = `${num1}`;
-        };
+        if (currentValue) {
+            // Scenario 1: Calculate the percentage of the current value
+            let percentageResult = parseFloat(currentValue) / 100;
+            currentValue = percentageResult.toString(); // Update currentValue
+            document.getElementById('result-display').innerText = formatter.format(percentageResult);
+            calculationHistory = `${currentValue} / 100 = ${formatter.format(percentageResult)}`;
+        } else if (num1 !== null && lastOperationCompleted) {
+            // Scenario 2: Apply percentage to the result of the last completed operation
+            let percentageResult = num1 / 100;
+            currentValue = percentageResult.toString(); // Update currentValue
+            document.getElementById('result-display').innerText = formatter.format(percentageResult);
+            calculationHistory = `${formatter.format(num1)} / 100 = ${formatter.format(percentageResult)}`;
+        } else if (num1 !== null && !lastOperationCompleted) {
+            // Scenario 3: Ongoing operation (e.g., "9 * 9"), apply percentage to num1
+            let percentageResult = num1 / 100;
+            currentValue = percentageResult.toString(); // Update currentValue
+            document.getElementById('result-display').innerText = formatter.format(percentageResult);
+            // Do not update calculationHistory for this intermediate step
+        }
+    
+        document.getElementById('calc-history').innerText = calculationHistory;
+        console.log(calculationHistory);
     });
-
 };
 
 document.addEventListener('DOMContentLoaded', setupCalculator);
